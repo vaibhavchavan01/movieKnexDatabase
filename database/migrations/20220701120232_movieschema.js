5b55851f7d48241b5e4225eb09f1c707cde5a158
vaibhavchavan01 <vaibhavchavan495@gmail.com>
2022-07-03 19:46:19 +0530

    making new model for user

--- database/migrations/20220701120232_movieschema.js
+++ database/migrations/20220701120232_movieschema.js
@@ -4,15 +4,6 @@
  */
 exports.up = function(knex) {
     return knex.schema
-    .createTable('users', function(table) {
-        table.increments('id').primary();
-        table.string('name', 255).notNullable();
-        table.string('email').notNullable().unique();
-        table.string('mobile').notNullable().unique();
-        table.string('password').notNullable();
-        table.boolean('is_admin').defaultTo(false);
-        table.timestamps(true, true);
-    })
     .createTable('genre', function(table){
         table.increments('id').primary();
         table.string('name', 50).notNullable();
@@ -57,10 +48,10 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
     return knex.schema
-    .dropTableIfExists("moviePeople")
+    .dropTableIfExists("movieActor")
     .dropTableIfExists("movieDirector")
     .dropTableIfExists("movie")
-        .dropTableIfExists("users")
+    .dropTableIfExists("users")
         .dropTableIfExists("genre")
         .dropTableIfExists("people")
 };
---
+++ database/migrations/20220703125242_userschema.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('users', function(table) {
+    table.increments('id').primary();
+    table.string('name', 255).notNullable();
+    table.string('email').notNullable().unique();
+    table.string('mobile').notNullable().unique();
+    table.string('password').notNullable();
+    table.boolean('is_admin').defaultTo(false);
+    table.timestamps(true, true);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('users');
+};
