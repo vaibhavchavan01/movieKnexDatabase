2e526210d210fad1d44bbe67795a364cdfaf89f0
vaibhavchavan01 <vaibhavchavan495@gmail.com>
2022-07-13 19:11:35 +0530

    added transaction and bookshelf in mmovie CRUD API

--- routes/forgetPassword.js
+++ routes/forgetPassword.js
@@ -19,7 +19,7 @@ router.post('/', async(req, res)=>{
                 })
         })     
     } catch (error) {
-        res.status(400).send(error)
+        return res.status(400).send({ Error: error.message })
     }
 })
 module.exports = router
