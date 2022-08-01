54b8a0a846b737c4973702e48ae243282d17ad26
vaibhavchavan01 <vaibhavchavan495@gmail.com>
2022-08-01 11:46:38 +0530

    changes in user file

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv').config();
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const cors = require('cors')
 
 //middleware for req.body
 app.use(express.urlencoded({ extended: false }))
 app.use(cors());
+app.use(express.json());
 
 const user = require('./routes/user');
 const genre = require('./routes/genre');
@@ -15,7 +16,7 @@ const people = require('./routes/people');
 const login = require('./routes/login')
 const forgetPassword = require('./routes/forgetPassword')
 //for json response
-app.use(express.json());
+
 
 app.use('/api/user', user);
 app.use('/api/genre', genre);
